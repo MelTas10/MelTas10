d69c67f3ed35dfc638699c109148705d3bd08b9d
Бекончик <lucevichnikita2023@gmail.com>
2025-03-04 14:13:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const loader = document.querySelector('.loader');
+    const container = document.querySelector('.container');
 
+    setTimeout(() => {
+        loader.style.opacity = 0;
+        setTimeout(() => {
+            loader.style.display = 'none';
+            container.style.display = 'block';
+            container.style.animation = 'fadeIn 0.5s';
+        }, 500);
+    }, 2000);
+
+    const infoButtons = document.querySelectorAll('.info-button');
+    infoButtons.forEach(button => {
+        button.addEventListener('mouseover', () => {
+            button.style.backgroundColor = '#7b1fa2';
+        });
+        button.addEventListener('mouseout', () => {
+            button.style.backgroundColor = '#4a148c';
+        });
+    });
+});
